214639eb52e8382a4f415ebaf10ea4003d2287dc
Pippen Wu <pipwu101@gmail.com>
2023-01-10 13:18:00 +0800

    run

--- lib/src/widgets/kg_carousel.dart
+++ lib/src/widgets/kg_carousel.dart
@@ -5,16 +5,13 @@ import 'package:flutter_svg/flutter_svg.dart';
 class KgCarousel extends StatefulWidget {
   const KgCarousel(
       {Key? key,
+      // each items contains three strings: the image path, the title, and the subtitle.
       this.items = const <String>[],
-      this.autoplay = true,
-      this.showNextButton = false,
-      this.widgetWhenDone})
+      this.autoplay = true})
       : super(key: key);
 
   final List items;
-  final bool autoplay, showNextButton;
-  final Widget?
-      widgetWhenDone; // this widget shows when 1) showNextButton is true, and 2) everything in the carousel has been seen.
+  final bool autoplay;
 
   @override
   State<KgCarousel> createState() => _KgCarouselState();
@@ -23,79 +20,68 @@ class KgCarousel extends StatefulWidget {
 class _KgCarouselState extends State<KgCarousel> {
   final CarouselController _controller = CarouselController();
 
-  int _current = 0;
-  bool hasSeenEverything = false;
-
   @override
   Widget build(BuildContext context) {
-    if (!widget.showNextButton || !hasSeenEverything) {
-      return Container(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            const Spacer(),
-            CarouselSlider(
-              items: widget.items
-                  .map(
-                    (e) => Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 48),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset(
-                            e[0],
-                            width: 166,
-                          ),
-                          const SizedBox(height: 24),
-                          Text(
-                            e[1],
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            e[2],
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                  .toList(),
-              carouselController: _controller,
-              options: CarouselOptions(
-                  height: MediaQuery.of(context).size.height - 2 * 24 - 300,
-                  autoPlay: widget.autoplay,
-                  autoPlayInterval: const Duration(seconds: 3),
-                  enlargeCenterPage: true,
-                  viewportFraction: 1,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  }),
-            ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(widget.items.length, (index) => index)
-                      .map(
-                        (e) => Container(
-                          width: 5,
-                          height: 5,
-                          margin: const EdgeInsets.symmetric(horizontal: 4),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                          ),
+    return Container(
+      child: Column(
+        children: [
+          CarouselSlider(
+            items: widget.items
+                .map(
+                  (e) => Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 48),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          e[0],
+                          width: 166,
                         ),
-                      )
-                      .toList(),
-                )),
-          ],
-        ),
-      );
-    } else {
-      return widget.widgetWhenDone!;
-    }
+                        const SizedBox(height: 24),
+                        Text(
+                          e[1],
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          e[2],
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+                .toList(),
+            carouselController: _controller,
+            options: CarouselOptions(
+                height: MediaQuery.of(context).size.height - 2 * 24 - 300,
+                autoPlay: widget.autoplay,
+                autoPlayInterval: const Duration(seconds: 3),
+                enlargeCenterPage: true,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {});
+                }),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(widget.items.length, (index) => index)
+                    .map(
+                      (e) => Container(
+                        width: 5,
+                        height: 5,
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              )),
+        ],
+      ),
+    );
   }
 }
